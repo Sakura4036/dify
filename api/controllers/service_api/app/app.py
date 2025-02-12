8cd9ede4dd7c0585322a7a8bc8d999836a6805e9
Hjw <1973140351@qq.com>
2025-02-12 15:25:11 +0800

    update app info api

--- api/controllers/service_api/app/app.py
+++ api/controllers/service_api/app/app.py
@@ -50,8 +50,7 @@ class AppInfoApi(Resource):
     @marshal_with(app_detail_fields)
     def get(self, app_model: App):
         """Get app information"""
-        tags = [tag.name for tag in app_model.tags]
-        return {"name": app_model.name, "description": app_model.description, "tags": tags}
+        return app_model
 
 
 api.add_resource(AppParameterApi, "/parameters")
