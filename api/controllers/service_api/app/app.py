68775fbfb57999399814d268b2f8c7ee99fdf4b9
Hjw <1973140351@qq.com>
2025-03-10 10:34:13 +0800

    init my changes

--- api/controllers/service_api/app/app.py
+++ api/controllers/service_api/app/app.py
@@ -5,6 +5,7 @@ from controllers.common import helpers as controller_helpers
 from controllers.service_api import api
 from controllers.service_api.app.error import AppUnavailableError
 from controllers.service_api.wraps import validate_app_token
+from fields.app_fields import app_detail_fields
 from models.model import App, AppMode
 from services.app_service import AppService
 
@@ -46,10 +47,10 @@ class AppMetaApi(Resource):
 
 class AppInfoApi(Resource):
     @validate_app_token
+    @marshal_with(app_detail_fields)
     def get(self, app_model: App):
         """Get app information"""
-        tags = [tag.name for tag in app_model.tags]
-        return {"name": app_model.name, "description": app_model.description, "tags": tags}
+        return app_model
 
 
 api.add_resource(AppParameterApi, "/parameters")
