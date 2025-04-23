10159f7cdf854fe0feacb361d2e033eec09738fa
Hjw <1973140351@qq.com>
2025-04-23 17:22:04 +0800

    add proxy env for start script

--- api/celery_start.sh
+++ api/celery_start.sh
@@ -2,4 +2,7 @@ cd ~/github/dify/api
 
 source activate dify
 
-nohup celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail,ops_trace --loglevel INFO > logs/run_celery_$(date +%y%m%d).log 2>&1 &
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+
+celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail,ops_trace --loglevel INFO | tee logs/run_celery_$(date +%y%m%d).log
--- api/flask_start.sh
+++ api/flask_start.sh
@@ -2,4 +2,7 @@ cd ~/github/dify/api
 
 source activate dify
 
-nohup flask run --host 0.0.0.0 --port=5001 --debug > logs/run_$(date +%y%m%d).log 2>&1 &
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+
+flask run --host 0.0.0.0 --port=5001 --debug | tee logs/run_$(date +%y%m%d).log
