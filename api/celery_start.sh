1c4a472f287d2cb174078d13edafd77616af21af
Sakura4036 <1973140351@qq.com>
2025-07-02 17:55:53 +0800

    use uv run instead of run python and celery directly

--- api/celery_start.sh
+++ api/celery_start.sh
@@ -2,7 +2,7 @@ cd ~/github/dify/api
 
 source activate dify
 
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=http://127.0.0.1:7890
+#export http_proxy=http://127.0.0.1:7890
+#export https_proxy=http://127.0.0.1:7890
 
-celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail,ops_trace --loglevel INFO | tee logs/run_celery_$(date +%y%m%d).log
+uv run celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail,ops_trace --loglevel INFO | tee logs/run_celery_$(date +%y%m%d).log
