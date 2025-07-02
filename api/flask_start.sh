abbf42cd163d019a3e91f0bef138bd46f33824ba
Sakura4036 <1973140351@qq.com>
2025-07-02 17:55:53 +0800

    use uv run instead of run python and celery directly

--- api/flask_start.sh
+++ api/flask_start.sh
@@ -5,4 +5,4 @@ source activate dify
 export http_proxy=http://127.0.0.1:7890
 export https_proxy=http://127.0.0.1:7890
 
-flask run --host 0.0.0.0 --port=5001 --debug | tee logs/run_$(date +%y%m%d).log
+uv run flask run --host 0.0.0.0 --port=5001 --debug | tee logs/run_$(date +%y%m%d).log
